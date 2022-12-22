535600765096119e734b792a86435d4c30845273
USER\DE <dalam0990@gmail.com>
2022-12-22 14:07:31 +0900

    [기능 추가] 공지 게시판 - 페이징 구현 중 - 디테일 구현 중

--- workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/controller/NoticeController.java
+++ workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/controller/NoticeController.java
@@ -8,7 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.coding5.el.common.page.PageVo;
+import com.coding5.el.common.page.Pagination;
 import com.coding5.el.notice.service.NoticeService;
 import com.coding5.el.notice.vo.NoticeVo;
 
@@ -16,13 +19,21 @@ import com.coding5.el.notice.vo.NoticeVo;
 @RequestMapping("notice")
 public class NoticeController {
 	
-	@Autowired private NoticeService ns;
+	@Autowired private NoticeService noticeService;
 	
 	//공지사항 리스트
 	@GetMapping("list")
-	public String list(Model model) {
+	public String list(@RequestParam(value = "cpage", defaultValue = "1") int currentPage, Model model) {
 		
-		List<NoticeVo> voList = ns.selectList();
+		int listCount = noticeService.selectListCount();
+
+		int pageLimit = 10;
+		int boardLimit = 5;
+
+		PageVo pv = Pagination.getPageVo(listCount, currentPage, pageLimit, boardLimit);
+		
+		List<NoticeVo> voList = noticeService.selectList(pv);
+		model.addAttribute("pv", pv);
 		model.addAttribute("voList", voList);
 		
 		return "notice/list";
@@ -36,7 +47,7 @@ public class NoticeController {
 	
 	@PostMapping("write")
 	public String write(NoticeVo vo) {
-		int result = ns.write(vo);
+		int result = noticeService.write(vo);
 		
 		if(result == 1) {
 			return "notice/detail";			
@@ -53,10 +64,16 @@ public class NoticeController {
 	
 	
 	
-	//공지사항 상세조회
-	@GetMapping("detail")
-	public String detail() {
-		return "notice/detail";
-	}
+	/*
+	 * //공지사항 상세조회
+	 * 
+	 * @GetMapping("detail") public String detail(int noticeNo, Model model) {
+	 * 
+	 * NoticeVo nv = noticeService.detail(noticeNo);
+	 * 
+	 * model.addAttribute("nv", nv);
+	 * 
+	 * return "notice/detail"; }
+	 */
 
 }
--- workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/dao/NoticeDao.java
+++ workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/dao/NoticeDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
+import com.coding5.el.common.page.PageVo;
 import com.coding5.el.notice.vo.NoticeVo;
 
 public interface NoticeDao {
@@ -11,11 +12,17 @@ public interface NoticeDao {
 	//공지사항 작성하기
 	public int write(SqlSessionTemplate sst, NoticeVo vo);
 	
-	//공지사항 갯수 업데이트
-	public int updateCnt(SqlSessionTemplate sst);
 	
 	//공지사항 목록 조회
-	public List<NoticeVo> selectList(SqlSessionTemplate sst);
+	public List<NoticeVo> selectList(SqlSessionTemplate sst, PageVo pv);
+
+	//공지사항 페이지 카운트
+	public int selectListCount(SqlSessionTemplate sst);
+
+	//공지사항 디테일
+	public NoticeVo detail(SqlSessionTemplate sst, int noticeNo);
+
+
 
 	
 }
