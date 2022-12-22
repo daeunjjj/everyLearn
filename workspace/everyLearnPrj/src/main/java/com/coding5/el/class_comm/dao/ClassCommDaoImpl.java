4eaeb53569dc5d1fef40aca5f6e86737e1a3d1a6
DESKTOP-2NV54CR\ayj71 <ayj7137@naver.com>
2022-12-22 14:14:40 +0900

    [DAO 수정] 강의 커뮤니티
    - dao 수정

--- workspace/everyLearnPrj/src/main/java/com/coding5/el/class_comm/dao/ClassCommDaoImpl.java
+++ workspace/everyLearnPrj/src/main/java/com/coding5/el/class_comm/dao/ClassCommDaoImpl.java
@@ -1,9 +1,11 @@
 package com.coding5.el.class_comm.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.coding5.el.class_comm.vo.ClassCommVo;
 
+@Repository
 public class ClassCommDaoImpl implements ClassCommDao{
 
 	//게시글 등록
