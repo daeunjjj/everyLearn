b7026f6b510b5cb8ac05a6d1af59507824d70ab2
USER\DE <dalam0990@gmail.com>
2022-12-23 13:41:31 +0900

    [서버구현/UI구현] 관리자 공지 리스트 - 페이징 구현 - 공지 삭제 구현 - 관리자 페이지에서 공지 리스트 보이게 UI 구현

--- workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/dao/NoticeDaoImpl.java
+++ workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/dao/NoticeDaoImpl.java
@@ -2,38 +2,49 @@ package com.coding5.el.notice.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.coding5.el.common.page.PageVo;
 import com.coding5.el.notice.vo.NoticeVo;
+import com.coding5.el.notice.vo.PageVo;
 
 @Repository
 public class NoticeDaoImpl implements NoticeDao {
+
+	@Autowired private SqlSessionTemplate sst;
+
+	@Override
+	public int write(NoticeVo vo) throws Exception {
+		//공지 작성
+		return sst.insert("noticeMapper.insertNotice", vo);
+	}
+
+	@Override
+	public List<NoticeVo> getNoticeList(PageVo pageVo) throws Exception {
+		//공지 리스트
+		return sst.selectList("noticeMapper.getNoticeList", pageVo);
+	}
+
+	@Override
+	public int deleteNotice(String[] delArr) throws Exception {
+		//공지 삭제
+		return sst.update("noticeMapper.deleteNotice", delArr);
+	}
+
+	@Override
+	public int getNoticeCnt() throws Exception {
+		//공지 갯수
+		return sst.selectOne("noticeMapper.getNoticeCnt");
+	}
+
+	@Override
+	public int editNotice(NoticeVo vo) throws Exception {
+		//공지 수정
+		return sst.update("noticeMapper.editNotice", vo);
+	}
+
 	
-	@Override
-	public int write(SqlSessionTemplate sst, NoticeVo vo) {
-		//공지사항 작성
-		return sst.insert("noticeMapper.write", vo);
-	}
-
-	@Override
-	public List<NoticeVo> selectList(SqlSessionTemplate sst, PageVo pv) {
-		//공지사항 목록 조회	
-		return sst.selectList("noticeMapper.selectList");
-	}
-
-	@Override
-	public int selectListCount(SqlSessionTemplate sst) {
-		//공지사항 리스트 카운트
-		return sst.selectOne("noticeMapper.selectListCount");
-	}
-
-	@Override
-	public NoticeVo detail(SqlSessionTemplate sst, int noticeNo) {
-		// 공지사항 디테일
-		return sst.selectOne("noticeMapper.detail", noticeNo);
-	}
-
+	
+	
 }
--- workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/service/NoticeService.java
+++ workspace/everyLearnPrj/src/main/java/com/coding5/el/notice/service/NoticeService.java
@@ -2,21 +2,24 @@ package com.coding5.el.notice.service;
 
 import java.util.List;
 
-import com.coding5.el.common.page.PageVo;
 import com.coding5.el.notice.vo.NoticeVo;
+import com.coding5.el.notice.vo.PageVo;
 
 public interface NoticeService {
 	
-	//공지사항 작성
-	public int write(NoticeVo vo);
+	//글쓰기
+	int write(NoticeVo vo) throws Exception;
+
+	//공지 리스트
+	List<NoticeVo> getNoticeList(PageVo pageVo) throws Exception;
+
+	//공지 삭제
+	int deleteNotice(String str) throws Exception;
 	
-	//공지사항 목록
-	public List<NoticeVo> selectList(PageVo pv);
+	//공지 수정
+	int editNotice(NoticeVo vo) throws Exception;
 
-	//공지사항 목록 갯수
-	public int selectListCount();
-
-	//공지사항 디테일
-	public NoticeVo detail(int noticeNo);
+	//공지 갯수
+	int getNoticeCnt() throws Exception;
 
 }
