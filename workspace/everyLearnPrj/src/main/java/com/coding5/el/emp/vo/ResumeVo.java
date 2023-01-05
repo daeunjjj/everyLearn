11a6452d4a28351fa8e6ddc2a0d6f87d0139dc2e
thdmsdl97 <thdmsdl97@gmail.com>
2023-01-05 12:05:44 +0900

    [서버] 이력서
    - 이력서 INSERT / UPDATE

--- workspace/everyLearnPrj/src/main/java/com/coding5/el/emp/vo/ResumeVo.java
+++ workspace/everyLearnPrj/src/main/java/com/coding5/el/emp/vo/ResumeVo.java
@@ -8,12 +8,19 @@ public class ResumeVo {
 	// 이력서
 	private String no;
 	private String memberNo;
-	private String adress;
+	private String name;
+	private String phone;
+	private String email;
+	private String address;
+	private String detail_address;
+	private String interest;
+	private String attatch;
 	private String link;
 	private String introduce;
 	private String enrollDate;
 	private String modifyDate;
 	
 	
-	
 }
+
+
